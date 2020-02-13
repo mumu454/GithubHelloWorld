fa8d3fe7d15211fcbd1453b148b3955ded75e052
Muna Chimoriya <muna.chimoriya.2022@student.lhschools.org>
2020-02-13 12:03:50 -0500

    Called Hello World Method

--- src/GithubHelloWorld.java
+++ src/GithubHelloWorld.java
@@ -1,12 +1,12 @@
 public class GithubHelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
-
+        helloWorld();
         
     }
 
     public static void helloWorld(){
-        System.out.println("Hello World");
+        System.out.println("Hello Github World");
     }
 
 
